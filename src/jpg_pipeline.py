a033bf8599bd3e878064c4844ed9d7f2e078f430
ljbelenky <ljbelenky@gmail.com>
2018-02-28 11:30:22 -0700

    streamlining jpg_pipeline

--- src/jpg_pipeline.py
+++ src/jpg_pipeline.py
@@ -5,15 +5,10 @@ import numpy as np
 This class loads a jpg and prepares it by cropping and sizing  in the manner used to train the neural network'''
 
 class jpgPipeline():
-    def __init__(self,filename,with_target = False, image_mode = 'RGB'):
-        self.fname = filename
-        self.image_mode =image_mode
-        self._load()
-
-    def _load(self):
-        self.image = Image.open(self.fname).convert(mode = self.image_mode)
-        box = self.image.getbbox()
-        image = self.image.crop(box)
+    def __init__(self,filename, image_mode = 'RGB'):
+        image = Image.open(filename).convert(mode = image_mode)
+        box = image.getbbox()
+        image = image.crop(box)
         w = box[2]-box[0]
         h = box[3]-box[1]
         a = min(w,h)*.9
@@ -23,7 +18,7 @@ class jpgPipeline():
         bottom = (h+a)/2
         cropped_image = image.crop((left,top,right,bottom))
         large_thumbnail=cropped_image.resize((200,200))
-        small_thumbnail=cropped_image.resize((150,150))
         large = np.expand_dims(np.asarray(large_thumbnail)/255,axis = 0)
+        small_thumbnail=cropped_image.resize((150,150))
         small = np.expand_dims(np.asarray(small_thumbnail)/255, axis = 0)
         self.X = {'large':large,'small':small}
